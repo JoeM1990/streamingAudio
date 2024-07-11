a284924d953c5943b9341eaf690c9b6737d2dd93
Joe Monkila <70669743+JoeM1990@users.noreply.github.com>
2024-07-11 08:52:57 +0100

    Update formatter.dart

--- lib/helper/formatter.dart
+++ lib/helper/formatter.dart
@@ -26,16 +26,16 @@ Map<String, dynamic> returnSongLayout(dynamic index, Video song) {
     'id': index,
     'ytid': song.id.toString(),
     'title': formatSongTitle(
-      song.title.split('-')[song.title.split('-').length - 1],
+       song.title.split('-')[song.title.split('-').length - 1],
     ),
     'image': song.thumbnails.standardResUrl,
     'lowResImage': song.thumbnails.lowResUrl,
     'highResImage': song.thumbnails.maxResUrl,
-    'album': '',
+    'album': ' ',
     'type': 'song',
     'more_info': {
-      'primary_artists': song.title.split('-')[0],
-      'singers': song.title.split('-')[0],
+       'primary_artists': song.title.split('-')[0],
+       'singers': song.title.split('-')[0],
     }
   };
 }
